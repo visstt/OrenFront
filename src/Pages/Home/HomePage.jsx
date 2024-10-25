959b5f90fa1ab0f7cdfeaddac793d226dfc7446f
Admin <@schckuratova.mar@yandex.ru>
2024-10-26 01:15:16 +0500

    add homePageSection1 v2

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomeSection1 from "./HomeSection1";
+import HomeSection1 from "./Section1/HomeSection1";
 export default function HomePage() {
   return (
     <div>
