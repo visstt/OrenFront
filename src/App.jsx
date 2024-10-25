fd475fc7deae29d00bbd8cb018268490abbd4dca
Admin <@schckuratova.mar@yandex.ru>
2024-10-26 01:46:12 +0500

    fix bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Catalog from "./Pages/Catalog/catalogPage";
 import HomePage from "./Pages/Home/HomePage";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Catalog />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </Router>
   );
 }
 
