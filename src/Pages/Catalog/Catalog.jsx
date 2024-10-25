93f23a3fbd6d3b01bf877bcddee058f69d00f431
FantomStudy <arzamastsevdaniil@gmail.com>
2024-10-26 00:36:51 +0300

    progress

--- src/Pages/Catalog/Catalog.jsx
+++ src/Pages/Catalog/Catalog.jsx
@@ -2,5 +2,16 @@ import styles from "./Catalog.module.css";
 import React from "react";
 
 export default function Catalog() {
-  return <div></div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.menu}>
+          <div className={styles.date}></div>
+          <div className={styles.range}></div>
+          <div className={styles.place}></div>
+          <div className={styles.buttons}></div>
+        </div>
+      </div>
+    </>
+  );
 }
