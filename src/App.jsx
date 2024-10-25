2d66c2362163d9aed8d34dad7b3c4410ac8f31a0
FantomStudy <arzamastsevdaniil@gmail.com>
2024-10-26 00:21:40 +0300

    Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Catalog from "./Pages/Catalog/Catalog";
 function App() {
   return (
     <>
       <Header />
+      <Catalog />
     </>
   );
 }
---
+++ src/Pages/Catalog/Catalog.jsx
@@ -0,0 +1,6 @@
+import styles from "./Catalog.module.css";
+import React from "react";
+
+export default function Catalog() {
+  return <div></div>;
+}
